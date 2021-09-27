017c21577e229fdc94abe1db4883072c31f976b6
dusartvict <dusartvict@eisti.eu>
2021-09-27 11:58:47 +0200

    Async function

--- index.js
+++ index.js
@@ -3,46 +3,90 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-axios
-  .get('https://services-web.u-cergy.fr/calendar/LdapLogin')
-  .then(res => {
-  
-    const creds = build_credentials(res);
-    const config = get_config(res);
+// axios
+//   .get('https://services-web.u-cergy.fr/calendar/LdapLogin')
+//   .then(res => {
 
-    axios
-      .post('https://services-web.u-cergy.fr/calendar/LdapLogin/Logon', creds, config)
-      .then(res => {
+//     const creds = build_credentials(res);
+//     const config = get_config(res);
 
-        const params = build_params();
+//     axios
+//       .post('https://services-web.u-cergy.fr/calendar/LdapLogin/Logon', creds, config)
+//       .then(res => {
 
-        axios
-          .post('https://services-web.u-cergy.fr/calendar/Home/GetCalendarData', params)
-          .then(res => {
-            // console.log(res);
-          })
-          .catch(err => {
-            console.log('err');
-          });
-      });
-  });
+//         const params = build_params();
+
+//         axios
+//           .post('https://services-web.u-cergy.fr/calendar/Home/GetCalendarData', params)
+//           .then(res => {
+//             // console.log(res);
+//           })
+//           .catch(err => {
+//             console.log('err');
+//           });
+//       });
+//   });
+
+async function main() {
+
+  const resToken = await getToken();
+  const creds = build_credentials(resToken);
+  const config = get_config(resToken);
+  const resLogon = await logon(creds, config);
+  const params = build_params();
+  const calendarData = await getCalendarData(params);
+}
+
+main();
+
 
 
 // =============================================================================================
 // =============================================================================================
 // =============================================================================================
+
+
+async function getToken() {
+  try {
+    const resp = await axios.get('https://services-web.u-cergy.fr/calendar/LdapLogin');
+    return resp;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+async function logon(creds, config) {
+  try {
+    const resp = await axios.post('https://services-web.u-cergy.fr/calendar/LdapLogin/Logon', creds, config);
+    return resp;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getCalendarData(params) {
+  try {
+    const resp = await axios.post('https://services-web.u-cergy.fr/calendar/Home/GetCalendarData', params);
+    return resp;
+  } catch (err) {
+    console.error(err);
+  }
+
+}
+
 
 
 function build_credentials(res) {
-  
+
   const token_starting_point = res.data.search('__RequestVerificationToken');
   const token = get_token_from_string(token_starting_point, res.data);
-  
+
   let creds = new URLSearchParams();
-  creds.append("Name", process.env.USERNAME);
+  creds.append("Name", process.env.NAME);
   creds.append("Password", process.env.PASSWORD);
   creds.append("__RequestVerificationToken", token);
-  
+
   return creds;
 }
 
@@ -57,10 +101,10 @@ function get_config(res) {
 
 function build_params() {
   const today = new Date();
-  const next_week = new Date(today.getFullYear(), today.getMonth(), today.getDate()+7);
+  const next_week = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7);
   const today_formated = today.toISOString().split('T')[0];
   const next_week_formated = next_week.toISOString().split('T')[0];
-  
+
   return {
     "start": today_formated,
     "end": next_week_formated,
